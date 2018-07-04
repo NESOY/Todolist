7b048df4f32798fec2eda5f14325eeefc297b9af
YoungJae Kwon <kyoje11@gmail.com>
2018-07-05 00:33:56 +0900

    Insert Data in SQLite3

--- todolist.py
+++ todolist.py
@@ -1,6 +1,29 @@
-# Input Command
-command = input()
-# Parse Command
+import sqlite3 as sqlite
+import sys
+def main():
+    command = input()
+    tokens = command.split()
+    init()
+    if(tokens[1] == "add"):
+        add(tokens[2])
 
-# Output Command
-print(command)
+def init():
+    conn = sqlite.connect("todolist.db")
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS task(description TEXT)")
+    print("Create Database")
+
+def add(param):
+    conn = sqlite.connect("todolist.db")	
+    with conn:
+        cur = conn.cursor()
+        sql = "INSERT INTO task(description) values (?)"
+        cur.execute(sql, [param])
+        conn.commit()
+    print("Success Save Task")
+
+
+
+if __name__ == "__main__":
+	main()
