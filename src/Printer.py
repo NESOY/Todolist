f8b63876fb01b7557a967292c0adf034d9e3becf
Kwon Young Jae <kyoje11@gmail.com>
2018-07-16 22:24:43 +0900

    Remove duplicate Code

--- src/Printer.py
+++ src/Printer.py
@@ -69,16 +69,5 @@ class color:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-
 class DeltaTemplate(Template):
     delimiter = "%"
-
-def strfdelta(tdelta, fmt):
-    d = {"D": tdelta.days}
-    hours, rem = divmod(tdelta.seconds, 3600)
-    minutes, seconds = divmod(rem, 60)
-    d["H"] = '{:02d}'.format(hours)
-    d["M"] = '{:02d}'.format(minutes)
-    d["S"] = '{:02d}'.format(seconds)
-    t = DeltaTemplate(fmt)
-    return t.substitute(**d)
